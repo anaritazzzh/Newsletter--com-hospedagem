0bda4e71ece2df5ae6eb30a0c0d82ba1c01c7ed5
anaritazzzh <anaritazzzh@gmail.com>
2026-04-02 10:29:09 -0300

    Iniciando javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+const root = document.documentElement;
+const btn = document.getElementById("btnTema");
+const ano = document.getElementById("ano");
+
+ano.textContent = new Date ().getFullYear()
+
+
+
